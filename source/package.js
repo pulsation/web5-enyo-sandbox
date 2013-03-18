21319a0a896e41d57ec15f307dc07d67e632f8c7
pulsation <philippe.sam-long@laposte.net>
2013-03-18 16:02:17 +0100

    mvvm: add pouchdb as a library to the project

--- source/package.js
+++ source/package.js
@@ -1,6 +1,8 @@
 enyo.depends(
 	"$lib/layout",
 	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
+    "$lib/pouchdb/pouchdb-nightly.js", // PouchDB external library
+    
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
 	"App.css",
 	"App.js"
